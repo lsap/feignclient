8f3580ce70f0028f212787a46ec3bbfd778c194e
serhii_kostyrko <comc49092@gmail.com>
2022-08-01 11:40:43 +0300

    Initial commit.

---
+++ src/main/java/org/met/connectedservices/FeignclientApplication.java
@@ -0,0 +1,13 @@
+package org.met.connectedservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/met/connectedservices/FeignclientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.met.connectedservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
