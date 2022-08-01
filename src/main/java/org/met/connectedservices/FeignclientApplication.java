54a7f1f16d46f4224748e24dccd1ecbeb00e287c
Serhii Kostyrko <serhii.kostyrko@192.168.1.5>
2022-08-01 13:53:35 +0300

    Enabled feign client.

--- src/main/java/org/met/connectedservices/FeignclientApplication.java
+++ src/main/java/org/met/connectedservices/FeignclientApplication.java
@@ -1,8 +1,10 @@
 package org.met.connectedservices;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClients
 @SpringBootApplication
 public class FeignclientApplication {
 
